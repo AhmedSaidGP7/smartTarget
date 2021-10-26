e1c661c95f978e6e400590c57c78be02a14fb000
AhmedSaidGP7 <ahmed.saidgp7@gmail.com>
2021-10-26 15:30:04 +0200

    front-page

--- functions.php
+++ functions.php
@@ -4,7 +4,6 @@ function smartTarget_regsiter_styles(){
     wp_enqueue_style('SmartTargetStyle', get_template_directory_uri() . "/assets/css/style.css", array(), '1.0', 'all');
     wp_enqueue_style('SmartTarget-bootstrap', "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" , array(), '1.0', 'all');
     wp_enqueue_style('SmartTarget-fontawesome', "https://use.fontawesome.com/releases/v5.15.4/css/all.css", array(), '1.0', 'all');
-    wp_enqueue_style('SmartTarget-bootstrap_js', "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js", array(), '1.0', 'all');
 
 }
 
