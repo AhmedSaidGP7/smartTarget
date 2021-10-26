2fbb785b04dbb6a6e298c9d6ecc380ebc08e4b2f
AhmedSaidGP7 <ahmed.saidgp7@gmail.com>
2021-10-26 15:10:36 +0200

    front-page

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 function smartTarget_regsiter_styles(){
-    wp_enqueue_style('SmartTargetStyle', get_template_directory_uri() . "assets/css/style.css", array(), '1.0', 'all');
+    wp_enqueue_style('SmartTargetStyle', get_template_directory_uri() . "/assets/css/style.css", array(), '1.0', 'all');
 }
 
 add_action('wp_enqueue_scripts', 'smartTarget_regsiter_styles');
