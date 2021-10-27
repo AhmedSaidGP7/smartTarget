18dc287571be81b96cbc35613189b7a359ca5734
AhmedSaidGP7 <ahmed.saidgp7@gmail.com>
2021-10-27 18:37:07 +0200

    menus

--- header.php
+++ header.php
@@ -45,6 +45,9 @@
               </button>
 
 
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+
               <?php
 
                 wp_nan_menu(
@@ -56,10 +59,7 @@
                     )
                 );
               ?>
-
-
-
-              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                   <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="index.html">Home</a>
