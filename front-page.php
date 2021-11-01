4ed99e7a1ae412b62573b2a8b666116c224e1163
AhmedSaidGP7 <ahmed.saidgp7@gmail.com>
2021-11-01 11:58:36 +0200

    logo and post

--- front-page.php
+++ front-page.php
@@ -3,7 +3,16 @@
 ?>
 
       <section id="" class="">
-      
+      <?php
+          if(have_posts())
+          {
+            while (have_posts())
+            {
+              the_post();
+            }
+          }
+
+      ?>
         </section>
 
      <?php
