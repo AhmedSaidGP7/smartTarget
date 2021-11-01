ab9a6636b32408bf67a9dfaec4d083afa064ea30
AhmedSaidGP7 <ahmed.saidgp7@gmail.com>
2021-11-02 00:25:34 +0200

    home

--- front-page.php
+++ front-page.php
@@ -2,19 +2,293 @@
     get_header();
 ?>
 
-      <section id="" class="">
-      <?php
-          if(have_posts())
-          {
-            while (have_posts())
-            {
-              the_post();
-              the_content();
-            }
-          }
 
-      ?>
-        </section>
+          <main id="home-main" class="homepagemainsection">
+            <div class="container">
+              <div class="row">
+                <div class="col-12">
+                  <div class="d-flex justify-content-center home-mainheadtitle">
+                    New Generation <br> Digital Agency
+                  </div>
+                </div>
+                <div class="col-12">
+                  <div  class="d-flex justify-content-center">
+                   <p class="home-maintext">Smart Target is a fully integrated digital marketing agency with more than 13 years experience in helping different businesses reach their ultimate goal.</p> 
+                  </div>
+                </div>
+              </div>
+            </div>
+          </main>
+
+          <section id="home-about" class="home-aboutussection">
+            <div class="container">
+              <div class="row">
+                <div class="col-12">
+                  <h2 class="home-about-haedline">ABOUT US</h2>
+                </div>
+                <div class="col-12 d-flex justify-content-center">
+                  <p class="home-about-text">Smart Target is a fully integrated digital marketing agency with more than 13 years experience in helping different businesses reach their ultimate goal. Our aim is to elevate online presence, build loyal online following, create useful digital content, and build long term relationships between brands and people.
+                    Our team of professionals in different fields are passionate about helping companies reach their business goals by converting more followers into customers, increase organic traffic from all social media platforms, improving conversion rates, and growing business based on engagement and meaningful interactions.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </section>
+
+          <section id="home-reasons" class="home-whysmarttargetsection">
+            <div class="container">
+              <div class="row d-flex justify-content-center">
+                <div class="col-12">
+                  <h1 class="home-whySmartH1">Why Smart Target</h1>
+                </div>
+                
+                <!--Start Card-->
+                <div class="col d-flex justify-content-center">
+                  <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 18rem;">
+                    <div class="card-body">
+                      <div class="d-flex justify-content-center">
+                        <img src="assets/images/why-us-icon1.png" alt="" class=""> 
+                      </div>
+                      <h5 class="card-title text-center home-whyus-reasontitle">13 Years Of Experience</h5>
+                      <p class="card-text text-center home-whyus-reason">Smart Target was established in 2008 with a strict focus on the growth of its client base and providing them with added value to transport them to the next level.</p>
+                    </div>
+                  </div>
+                </div>
+                <!--End Card-->
+
+                <!--Start Card-->
+                <div class="col d-flex justify-content-center">
+                  <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 18rem;">
+                    <div class="card-body">
+                      <div class="d-flex justify-content-center">
+                        <img src="assets/images/why-us-icon1.png" alt="" class=""> 
+                      </div>
+                      <h5 class="card-title text-center home-whyus-reasontitle">13 Years Of Experience</h5>
+                      <p class="card-text text-center home-whyus-reason">Smart Target was established in 2008 with a strict focus on the growth of its client base and providing them with added value to transport them to the next level.</p>
+                    </div>
+                  </div>
+                </div>
+                <!--End Card-->
+
+                <!--Start Card-->
+                <div class="col d-flex justify-content-center">
+                  <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 18rem;">
+                    <div class="card-body">
+                      <div class="d-flex justify-content-center">
+                        <img src="assets/images/why-us-icon1.png" alt="" class=""> 
+                      </div>
+                      <h5 class="card-title text-center home-whyus-reasontitle">13 Years Of Experience</h5>
+                      <p class="card-text text-center home-whyus-reason">Smart Target was established in 2008 with a strict focus on the growth of its client base and providing them with added value to transport them to the next level.</p>
+                    </div>
+                  </div>
+                </div>
+                <!--End Card-->
+
+                <!--Start Card-->
+                <div class="col d-flex justify-content-center">
+                  <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 18rem;">
+                    <div class="card-body">
+                      <div class="d-flex justify-content-center">
+                        <img src="assets/images/why-us-icon1.png" alt="" class=""> 
+                      </div>
+                      <h5 class="card-title text-center home-whyus-reasontitle">13 Years Of Experience</h5>
+                      <p class="card-text text-center home-whyus-reason">Smart Target was established in 2008 with a strict focus on the growth of its client base and providing them with added value to transport them to the next level.</p>
+                    </div>
+                  </div>
+                </div>
+                <!--End Card-->
+
+              </div>
+            </div>
+            </section>
+
+            <section class="home-whatwedosection" id="ourservices">
+              <div class="container">
+                <div class="row">
+                  <div class="col-12">
+                    <h1 class="home-whatwedoh1">WHAT WE DO</h1>
+                  </div>
+                  <!--Start Card-->
+                <div class="col d-flex justify-content-center">
+                  <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 18rem;">
+                    <div class="card-body">
+                      <div class="d-flex justify-content-center">
+                        <img src="assets/images/development-img.png" alt="" class=""> 
+                      </div>
+                      <h5 class="card-title text-center home-whatwedotitle">Creative Branding</h5>
+        
+                    </div>
+                  </div>
+                </div>
+                <!--End Card-->
+
+                <!--Start Card-->
+                <div class="col d-flex justify-content-center">
+                  <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 18rem;">
+                    <div class="card-body">
+                      <div class="d-flex justify-content-center">
+                        <img src="assets/images/development-img.png" alt="" class=""> 
+                      </div>
+                      <h5 class="card-title text-center home-whatwedotitle">Creative Branding</h5>
+        
+                    </div>
+                  </div>
+                </div>
+                <!--End Card-->
+
+                <!--Start Card-->
+                <div class="col d-flex justify-content-center">
+                  <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 18rem;">
+                    <div class="card-body">
+                      <div class="d-flex justify-content-center">
+                        <img src="assets/images/development-img.png" alt="" class=""> 
+                      </div>
+                      <h5 class="card-title text-center home-whatwedotitle">Creative Branding</h5>
+        
+                    </div>
+                  </div>
+                </div>
+                <!--End Card-->
+
+
+                <!--Start Card-->
+                <div class="col d-flex justify-content-center">
+                  <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 18rem;">
+                    <div class="card-body">
+                      <div class="d-flex justify-content-center">
+                        <img src="assets/images/development-img.png" alt="" class=""> 
+                      </div>
+                      <h5 class="card-title text-center home-whatwedotitle">Creative Branding</h5>
+        
+                    </div>
+                  </div>
+                </div>
+                <!--End Card-->
+
+                <!--Start Card-->
+                <div class="col d-flex justify-content-center">
+                  <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 18rem;">
+                    <div class="card-body">
+                      <div class="d-flex justify-content-center">
+                        <img src="assets/images/development-img.png" alt="" class=""> 
+                      </div>
+                      <h5 class="card-title text-center home-whatwedotitle">Creative Branding</h5>
+        
+                    </div>
+                  </div>
+                </div>
+                <!--End Card-->
+
+                <!--Start Card-->
+                <div class="col d-flex justify-content-center">
+                  <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 18rem;">
+                    <div class="card-body">
+                      <div class="d-flex justify-content-center">
+                        <img src="assets/images/development-img.png" alt="" class=""> 
+                      </div>
+                      <h5 class="card-title text-center home-whatwedotitle">Creative Branding</h5>
+        
+                    </div>
+                  </div>
+                </div>
+                <!--End Card-->
+
+                <!--Start Card-->
+                <div class="col d-flex justify-content-center">
+                  <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 18rem;">
+                    <div class="card-body">
+                      <div class="d-flex justify-content-center">
+                        <img src="assets/images/development-img.png" alt="" class=""> 
+                      </div>
+                      <h5 class="card-title text-center home-whatwedotitle">Creative Branding</h5>
+        
+                    </div>
+                  </div>
+                </div>
+                <!--End Card-->
+
+                <!--Start Card-->
+                <div class="col d-flex justify-content-center">
+                  <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 18rem;">
+                    <div class="card-body">
+                      <div class="d-flex justify-content-center">
+                        <img src="assets/images/development-img.png" alt="" class=""> 
+                      </div>
+                      <h5 class="card-title text-center home-whatwedotitle">Creative Branding</h5>
+        
+                    </div>
+                  </div>
+                </div>
+                <!--End Card-->
+                </div>
+              </div>
+            </section>
+
+          <section id="home-clients" class="home-happyclients">
+            <div class="container">
+              <div class="row">
+                <div class="col-12">
+                  <h2 class="home-h2-happyclients">We have 96 happy clients</h2>
+                </div>
+
+                <!-- Start logo -->
+                <div class="col d-flex justify-content-center align-middle">
+                  <img src="assets/images/1.png" alt="" class="home-ourclients">
+                </div>
+                <!--End logo-->
+
+                <!-- Start logo -->
+                <div class="col d-flex justify-content-center align-middle">
+                  <img src="assets/images/2.png" alt="" class="home-ourclients">
+                </div>
+                <!--End logo-->
+
+                <!-- Start logo -->
+                <div class="col d-flex justify-content-center align-middle">
+                  <img src="assets/images/3.png" alt="" class="home-ourclients">
+                </div>
+                <!--End logo-->
+
+
+                <!-- Start logo -->
+                <div class="col d-flex justify-content-center align-middle">
+                  <img src="assets/images/4.png" alt="" class="home-ourclients">
+                </div>
+                <!--End logo-->
+
+                <!-- Start logo -->
+                <div class="col d-flex justify-content-center align-middle">
+                  <img src="assets/images/5.png" alt="" class="home-ourclients">
+                </div>
+                <!--End logo-->
+
+                <!-- Start logo -->
+                <div class="col d-flex justify-content-center align-middle">
+                  <img src="assets/images/6.png" alt="" class="home-ourclients">
+                </div>
+                <!--End logo-->
+
+                <!-- Start logo -->
+                <div class="col d-flex justify-content-center align-middle">
+                  <img src="assets/images/7.png" alt="" class="home-ourclients">
+                </div>
+                <!--End logo-->
+
+                <!-- Start logo -->
+                <div class="col d-flex justify-content-center align-middle">
+                  <img src="assets/images/8.png" alt="" class="home-ourclients">
+                </div>
+                <!--End logo-->
+
+                <!-- Start logo -->
+                <div class="col d-flex justify-content-center align-middle">
+                  <img src="assets/images/9.png" alt="" class="home-ourclients">
+                </div>
+                <!--End logo-->
+                
+              </div>
+            </div>
+          </section>
 
      <?php
         get_footer();
