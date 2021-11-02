f57514bc7e04bfe0f6d86bc199e4421511eaabc3
AhmedSaidGP7 <ahmed.saidgp7@gmail.com>
2021-11-02 09:25:56 +0200

    home services

--- front-page.php
+++ front-page.php
@@ -114,7 +114,7 @@
                   <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 18rem;">
                     <div class="card-body">
                       <div class="d-flex justify-content-center">
-                        <img src="wp-content/themes/smarttarget/assets/images/branding.png" alt="" class=""> 
+                        <img src="wp-content/themes/smarttarget/assets/images/branding.png" alt="" class="smarttarget-home-servicesicon"> 
                       </div>
                       <h5 class="card-title text-center home-whatwedotitle">Creative Branding</h5>
                       <p class="card-text text-center home-whyus-reason">Branding is what transforms first-time buyers into lifetime customers and turns an indifferent audience into brand evangelists.</p>
@@ -124,96 +124,111 @@
                 </div>
                 <!--End Card-->
 
-                  <!--Start Card-->
-                  <div class="col d-flex justify-content-center">
+                 <!--Start Card-->
+                 <div class="col d-flex justify-content-center">
                   <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 18rem;">
                     <div class="card-body">
                       <div class="d-flex justify-content-center">
-                        <img src="wp-content/themes/smarttarget/assets/images/branding.png" alt="" class=""> 
+                        <img src="wp-content/themes/smarttarget/assets/images/voiceover.png" alt="" class="smarttarget-home-servicesicon"> 
                       </div>
-                      <h5 class="card-title text-center home-whatwedotitle">Creative Branding</h5>
-                      <p class="card-text text-center home-whyus-reason">Branding is what transforms first-time buyers into lifetime customers and turns an indifferent audience into brand evangelists.</p>
+                      <h5 class="card-title text-center home-whatwedotitle">Voice Over</h5>
+                      <p class="card-text text-center home-whyus-reason">One of the greatest ways to tell your story is through audio. and we’ll help you to produce a high quality audio that features your business story and goals.</p>
         
                     </div>
                   </div>
                 </div>
                 <!--End Card-->
 
-                  <!--Start Card-->
-                  <div class="col d-flex justify-content-center">
+                 <!--Start Card-->
+                 <div class="col d-flex justify-content-center">
                   <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 18rem;">
                     <div class="card-body">
                       <div class="d-flex justify-content-center">
-                        <img src="wp-content/themes/smarttarget/assets/images/branding.png" alt="" class=""> 
+                        <img src="wp-content/themes/smarttarget/assets/images/social.png" alt="" class="smarttarget-home-servicesicon"> 
                       </div>
-                      <h5 class="card-title text-center home-whatwedotitle">Creative Branding</h5>
-                      <p class="card-text text-center home-whyus-reason">Branding is what transforms first-time buyers into lifetime customers and turns an indifferent audience into brand evangelists.</p>
+                      <h5 class="card-title text-center home-whatwedotitle">Digital Marketing</h5>
+                      <p class="card-text text-center home-whyus-reason">When Visual Content is aligned with your branding, you surely stand out from the crowd. Vivacious designs nowadays are not optional.</p>
         
                     </div>
                   </div>
                 </div>
                 <!--End Card-->
 
-                  <!--Start Card-->
-                  <div class="col d-flex justify-content-center">
+                 <!--Start Card-->
+                 <div class="col d-flex justify-content-center">
                   <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 18rem;">
                     <div class="card-body">
                       <div class="d-flex justify-content-center">
-                        <img src="wp-content/themes/smarttarget/assets/images/branding.png" alt="" class=""> 
+                        <img src="wp-content/themes/smarttarget/assets/images/ads.png" alt="" class="smarttarget-home-servicesicon"> 
                       </div>
-                      <h5 class="card-title text-center home-whatwedotitle">Creative Branding</h5>
-                      <p class="card-text text-center home-whyus-reason">Branding is what transforms first-time buyers into lifetime customers and turns an indifferent audience into brand evangelists.</p>
+                      <h5 class="card-title text-center home-whatwedotitle">Advertising</h5>
+                      <p class="card-text text-center home-whyus-reason">What happens on digital, stays on search engines for ever! Our SEO strategies will place your business among the titans.</p>
         
                     </div>
                   </div>
                 </div>
                 <!--End Card-->
 
-                  <!--Start Card-->
-                  <div class="col d-flex justify-content-center">
+                <!--Start Card-->
+                <div class="col d-flex justify-content-center">
                   <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 18rem;">
                     <div class="card-body">
                       <div class="d-flex justify-content-center">
-                        <img src="wp-content/themes/smarttarget/assets/images/branding.png" alt="" class=""> 
+                        <img src="wp-content/themes/smarttarget/assets/images/camera.png" alt="" class="smarttarget-home-servicesicon"> 
                       </div>
-                      <h5 class="card-title text-center home-whatwedotitle">Creative Branding</h5>
-                      <p class="card-text text-center home-whyus-reason">Branding is what transforms first-time buyers into lifetime customers and turns an indifferent audience into brand evangelists.</p>
+                      <h5 class="card-title text-center home-whatwedotitle">Photography & Videos</h5>
+                      <p class="card-text text-center home-whyus-reason">Photos and videos are the most important communication platform to capture attention without too much words. We make sure to tell your story through high quality visual content.</p>
         
                     </div>
                   </div>
                 </div>
                 <!--End Card-->
-
-                  <!--Start Card-->
-                  <div class="col d-flex justify-content-center">
+               
+                <!--Start Card-->
+                <div class="col d-flex justify-content-center">
                   <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 18rem;">
                     <div class="card-body">
                       <div class="d-flex justify-content-center">
-                        <img src="wp-content/themes/smarttarget/assets/images/branding.png" alt="" class=""> 
+                        <img src="wp-content/themes/smarttarget/assets/images/bullhorn.png" alt="" class="smarttarget-home-servicesicon"> 
                       </div>
-                      <h5 class="card-title text-center home-whatwedotitle">Creative Branding</h5>
-                      <p class="card-text text-center home-whyus-reason">Branding is what transforms first-time buyers into lifetime customers and turns an indifferent audience into brand evangelists.</p>
+                      <h5 class="card-title text-center home-whatwedotitle">Social Media Marketing</h5>
+                      <p class="card-text text-center home-whyus-reason">Social media is a two-way form of communication that allows users to interact with the information being transmitted.</p>
         
                     </div>
                   </div>
                 </div>
                 <!--End Card-->
-
-                  <!--Start Card-->
-                  <div class="col d-flex justify-content-center">
+               
+                 <!--Start Card-->
+                 <div class="col d-flex justify-content-center">
                   <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 18rem;">
                     <div class="card-body">
                       <div class="d-flex justify-content-center">
-                        <img src="wp-content/themes/smarttarget/assets/images/branding.png" alt="" class=""> 
+                        <img src="wp-content/themes/smarttarget/assets/images/content.png" alt="" class="smarttarget-home-servicesicon"> 
                       </div>
-                      <h5 class="card-title text-center home-whatwedotitle">Creative Branding</h5>
-                      <p class="card-text text-center home-whyus-reason">Branding is what transforms first-time buyers into lifetime customers and turns an indifferent audience into brand evangelists.</p>
+                      <h5 class="card-title text-center home-whatwedotitle">Content Creation & Strategy</h5>
+                      <p class="card-text text-center home-whyus-reason">Content is the reason why search began in the first place. Today’s stories are digital & we make your stories worth reading.</p>
         
                     </div>
                   </div>
                 </div>
                 <!--End Card-->
 
+                
+                 <!--Start Card-->
+                 <div class="col d-flex justify-content-center">
+                  <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 18rem;">
+                    <div class="card-body">
+                      <div class="d-flex justify-content-center">
+                        <img src="wp-content/themes/smarttarget/assets/images/content.png" alt="" class="smarttarget-home-servicesicon"> 
+                      </div>
+                      <h5 class="card-title text-center home-whatwedotitle">Web Design & Development</h5>
+                      <p class="card-text text-center home-whyus-reason">Your website is the core image of your business. For this we will make sure you have a well designed, attractive and user friendly platform for an upgraded browsing experience.</p>
+        
+                    </div>
+                  </div>
+                </div>
+                <!--End Card-->
               
                 </div>
               </div>
